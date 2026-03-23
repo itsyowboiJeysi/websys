588fe9699260dd09bd9114c2b9a019e0bea86107
JOHN MICHAEL TOLDANES <johntoldanes@my.cspc.edu.ph>
2026-03-23 13:24:50 +0800

    add all project files

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+// Set port
+const PORT = 3000;
+
+// Middleware (optional)
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello, Express server is running!');
+});
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Welcome to the API' });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
